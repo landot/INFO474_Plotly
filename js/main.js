bd04a8be1f3ae7d8b06c6fb4ca19efd540589239
Timothy Landowski <timowland@gmail.com>
2016-04-13 00:32:30 -0700

    no message

--- js/main.js
+++ js/main.js
@@ -1,8 +1,97 @@
+'use strict';
+
+var bactNames = 
+['Aerobacter aerogenes',
+ 'Brucella abortus',
+ 'Brucella anthracis',
+ 'Diplococcus pneumoniae',
+ 'Escherichia coli',
+ 'Klebsiella pneumoniae',
+ 'Mycobacterium tuberculosis',
+ 'Proteus vulgaris',
+ 'Pseudomonas aeruginosa ',
+ 'Salmonella (Eberthella) typhosa ',
+ 'Salmonella schottmuelleri ',
+ 'Staphylococcus albus ',
+ 'Staphylococcus aureus ',
+ 'Streptococcus fecalis ',
+ 'Streptococcus hemolyticus ',
+ 'Streptococcus viridans']
+var bactPen = 
+    [870.0,
+    1.0,
+    0.001,
+    0.0050000000000000001,
+    100.0,
+    850.0,
+    800.0,
+    3.0,
+    850.0,
+    1.0,
+    10.0,
+    0.0069999999999999993,
+    0.029999999999999999,
+    1.0,
+    0.001,
+    0.0050000000000000001]
+var bactStre = [1.0,
+ 2.0,
+ 0.01,
+ 11.0,
+ 0.40000000000000002,
+ 1.2,
+ 5.0,
+ 0.10000000000000001,
+ 2.0,
+ 0.40000000000000002,
+ 0.80000000000000004,
+ 0.10000000000000001,
+ 0.029999999999999999,
+ 1.0,
+ 14.0,
+ 10.0]
+var bactNeo = [1.6000000000000001,
+ 0.02,
+ 0.0069999999999999993,
+ 10.0,
+ 0.10000000000000001,
+ 1.0,
+ 2.0,
+ 0.10000000000000001,
+ 0.40000000000000002,
+ 0.0080000000000000002,
+ 0.089999999999999997,
+ 0.001,
+ 0.001,
+ 0.10000000000000001,
+ 10.0,
+ 40.0]
+var bactGram = [
+    'negative',
+ 'negative',
+ 'positive',
+ 'positive',
+ 'negative',
+ 'negative',
+ 'negative',
+ 'negative',
+ 'negative',
+ 'negative',
+ 'negative',
+ 'positive',
+ 'positive',
+ 'positive',
+ 'positive',
+ 'positive'
+]
+
+console.log(bactNeo)
+
 var trace1 = {
     x: [1, 2, 3, 4],
-    y: [10, 15, 13, 17],
-    mode: 'markers',
-    type: 'scatter'
+    y: bactNames,
+    type: 'heatmap',
+    showscale: true
 };
 
 var trace2 = {
