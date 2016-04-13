a92ea9e859a1a9922d81f238f5173d5717f2fcd5
Timothy Landowski <timowland@gmail.com>
2016-04-13 03:03:46 -0700

    2 graphs done

--- js/main.js
+++ js/main.js
@@ -1,119 +1,111 @@
 'use strict';
 
-var bactNames = 
-['Aerobacter aerogenes',
- 'Brucella abortus',
- 'Brucella anthracis',
- 'Diplococcus pneumoniae',
- 'Escherichia coli',
- 'Klebsiella pneumoniae',
- 'Mycobacterium tuberculosis',
- 'Proteus vulgaris',
- 'Pseudomonas aeruginosa ',
- 'Salmonella (Eberthella) typhosa ',
- 'Salmonella schottmuelleri ',
- 'Staphylococcus albus ',
- 'Staphylococcus aureus ',
- 'Streptococcus fecalis ',
- 'Streptococcus hemolyticus ',
- 'Streptococcus viridans']
-var bactPen = 
-    [870.0,
-    1.0,
-    0.001,
-    0.0050000000000000001,
-    100.0,
-    850.0,
-    800.0,
-    3.0,
-    850.0,
-    1.0,
-    10.0,
-    0.0069999999999999993,
-    0.029999999999999999,
-    1.0,
-    0.001,
-    0.0050000000000000001]
-var bactStre = [1.0,
- 2.0,
- 0.01,
- 11.0,
- 0.40000000000000002,
- 1.2,
- 5.0,
- 0.10000000000000001,
- 2.0,
- 0.40000000000000002,
- 0.80000000000000004,
- 0.10000000000000001,
- 0.029999999999999999,
- 1.0,
- 14.0,
- 10.0]
-var bactNeo = [1.6000000000000001,
- 0.02,
- 0.0069999999999999993,
- 10.0,
- 0.10000000000000001,
- 1.0,
- 2.0,
- 0.10000000000000001,
- 0.40000000000000002,
- 0.0080000000000000002,
- 0.089999999999999997,
- 0.001,
- 0.001,
- 0.10000000000000001,
- 10.0,
- 40.0]
-var bactGram = [
-    'negative',
- 'negative',
- 'positive',
- 'positive',
- 'negative',
- 'negative',
- 'negative',
- 'negative',
- 'negative',
- 'negative',
- 'negative',
- 'positive',
- 'positive',
- 'positive',
- 'positive',
- 'positive'
-]
+//CHART 1 DATA (grouped bar chart)
+//mean drug use for each bacteria (gram positive)
+var data1 = [0.15, 5.16, 8.59]
 
-console.log(bactNeo)
+//mean drug use for each bacteria (gram negative)
+var data2 = [387.22, 1.43, 0.59]
+
+//OR
+//median drug use for each bacteria (gram positive)
+var data1b = [0.005, 1.0, 0.1]
+
+//median drug use for each bacteria (gram negative)
+var data2b = [100.0,1.0,.1]
+
+
+//CHART 2 DATA (bar chart)
+//mean effectiveness of each drug
+var data3 = [217.88, 3.07, 4.09]
+
+//OR
+//median effectiveness of each drug
+var data3b = [1.0, 1.0, 0.1]
+
+//CHART 3 DATA (grouped bar chart)
+//(3 groupings of data)
+var groupings = ['0-.99', '1-9.99', '10+']
+//Penicilin 
+var data4 = [6,4,6]
+//Streptomycin
+var data5 = [7,6,3]
+//Neomycin
+var data6 = [10,3,3]
+
+//CHART 4 DATA
+//box plot of average of drug's effect on bacteria grouped by gram staining
+//gram positive
+var data7 = [.01,7.00,.04,.02,.7,8.00,16.67]
+//gram negative
+var data8 = [290.87,1.01,33.5,284.07,269,1.07,284.13,.47,3.63]
+
+//drug names
+var drugNames = ['Penicilin', 'Streptomycin', 'Neomycin']
+
 
 var trace1 = {
-    x: [1, 2, 3, 4],
-    y: bactNames,
-    type: 'heatmap',
-    showscale: true
+    x: groupings,
+    y: [data4[0], data4[1], data4[2]],
+    name: drugNames[0],
+    type: 'bar'
 };
 
 var trace2 = {
-    x: [2, 3, 4, 5],
-    y: [16, 5, 11, 9],
-    mode: 'lines',
-    type: 'scatter'
+    x: groupings,
+    y: [data5[0], data5[1], data5[2]],
+    name: drugNames[1],
+    type: 'bar'
 };
 
 var trace3 = {
-    x: [1, 2, 3, 4],
-    y: [12, 9, 15, 12],
-    mode: 'lines+markers',
-    type: 'scatter'
+    x: groupings,
+    y: [data6[0], data6[1], data6[2]],
+    name: drugNames[2],
+    type: 'bar'
 };
 
-var data = [trace1, trace2, trace3];
+var data = [trace1, trace2, trace3]
 
 var layout = {
-    title: 'Line Plot',
-    height: window.height,
-    width: window.width
+    barmode: 'group',
+    title: 'General Effectiveness of Antibiotics on Bacteria',
+    xaxis: {
+        title: 'Minimum inhibitory concentration (MIC) of drug needed to prevent bacterial growth'    
+    },
+    yaxis: {
+        title: 'Types of bacteria'
+    }
+};
+var trace4 = {
+    y: data7,
+    type: 'box',
+    marker: {color: 'blue'},
+    name: 'Positive Gram Staining'
 };
 
-Plotly.newPlot('graph', data, layout);
+var trace5 = {
+    y: data8,  
+    type: 'box',
+    marker: {color: 'red'},
+    name: 'Negatives Gram Staining',
+};
+
+var boxplot_Layout = {
+    autosize: false,
+    width: 350,
+    height: 500,
+    yaxis: {
+    title: 'Minimum inhibitory concentration (MIC)',
+    zeroline: false
+  }
+}
+
+var boxplot1 = [trace4]
+var boxplot2 = [trace5]
+
+Plotly.newPlot('graph1', data, layout);
+Plotly.newPlot('graph2', boxplot1, boxplot_Layout);
+Plotly.newPlot('graph3', boxplot2, boxplot_Layout);
+
+
